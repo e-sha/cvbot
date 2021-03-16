366792c66cb5b4f1ecd0a0f0b89478b923ef0985
Eugene Shalnov <shalnov.eugen@gmail.com>
2021-03-17 00:05:52 +0300

    bugfix

--- utils/logger.py
+++ utils/logger.py
@@ -4,7 +4,7 @@ from ..src.logger import BaseLogger
 class Logger(BaseLogger):
     def __init__(self, name, processor):
         super().__init__(name)
-        self._log_Processor = processor
+        self._log_processor = processor
 
     def get_stat(self, num_rows=10):
         return self._log_processor(num_rows)
